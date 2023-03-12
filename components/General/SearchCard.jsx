5113c390d020f828f67eef89fba50442817aa405
Sajeesh007 <sajeesh8112001@gmial.com>
2023-03-12 22:10:28 +0530

    public availability

--- components/General/SearchCard.jsx
+++ components/General/SearchCard.jsx
@@ -1,94 +1,233 @@
 import { useForm } from "react-hook-form";
 import { useState } from "react";
 
-import { fetchByEmail } from "@/util/supabase.helper";
+import { fetchByEmail, handleEdit } from "@/util/supabase.helper";
 
 import Input from "@/components/Form/Input";
 import PersonalDetails from "./PersonalDetails";
 import CertificateDetails from "./CertificateDetails";
 
 export default function SearchCard() {
+  const {
+    register,
+    formState: { errors },
+    handleSubmit,
+  } = useForm();
 
-  const { register,formState : { errors }, handleSubmit} = useForm()
-
-  const [groupData, setgroupData] = useState(null)
-  const [singeData, setsingleData] = useState(null)
-  const [error, seterror] = useState(false)
-  const [loading, setloading] = useState(false)
+  const [groupData, setgroupData] = useState(null);
+  const [singeData, setsingleData] = useState(null);
+  const [error, seterror] = useState(false);
+  const [loading, setloading] = useState(false);
+  const [show, setshow] = useState(false);
 
   const onSubmit = async (data) => {
-    seterror(false)
-    await fetchByEmail(data.email, setsingleData, setgroupData, seterror, setloading)
-  }
+    seterror(false);
+    setshow(false);
+    setgroupData(null);
+    setsingleData(null);
+    await fetchByEmail(
+      data.email,
+      setsingleData,
+      setgroupData,
+      seterror,
+      setloading
+    );
+  };
 
   return (
-    <div className="flex flex-col space-y-6 justify-center items-center px-8 
-    text-white  bg-zinc-900 rounded-3xl mx-2 py-6 ">
-
-      <div className="flex flex-col space-y-6 justify-center items-center">
+    <div
+      className="flex flex-col space-y-6 justify-center items-center
+    text-white  mx-2  "
+    >
+      <div className="flex flex-col space-y-2 justify-center items-center bg-zinc-900 rounded-3xl p-5">
         <h2 className="font-man">Certificates</h2>
-        <form onSubmit={handleSubmit(onSubmit)} className='flex flex-col space-y-4'>
-            <Input type='email' label='College Mail ID' htmlFor='email' error={errors.email} register={register} required={true} />
-            
-              {error && <p className="pb-4 text-yellow-600">
-                Please check the mail id you entered or there is no participation record exist.
-              </p>}
+        <form
+          onSubmit={handleSubmit(onSubmit)}
+          className="flex flex-col space-y-4"
+        >
+          <Input
+            type="email"
+            label="College Mail ID"
+            htmlFor="email"
+            error={errors.email}
+            register={register}
+            required={true}
+          />
 
-            { loading ?
-              <button className="submit bg-green-500">
-                Loading...
-              </button>
-            :
-            <input type='submit' value='Submit' className='submit bg-green-500'/> }
-        
+          {error && (
+            <p className=" text-yellow-600">
+              Please check the mail id you entered or there is no participation
+              record exist. If it is an error please contact &nbsp;
+              <a
+                className="text-yellow-600 font-bold underline"
+                target="_blank"
+                rel="noreferrer"
+                href="https://wa.me/916238065153"
+              >
+                wa.me/916238065153
+              </a>
+            </p>
+          )}
+
+          {loading ? (
+            <button className="submit bg-green-500">Loading...</button>
+          ) : (
+            <input
+              type="submit"
+              value="Submit"
+              className="submit bg-green-500"
+            />
+          )}
         </form>
       </div>
 
-      {(singeData || groupData ) && <div className='flex flex-col space-y-4'>
-        <h3>Validate Details</h3>
+      {(singeData || groupData) && (
+        <div className="flex flex-col space-y-4 bg-zinc-900 rounded-3xl  p-5">
+          <h3>Validate Details</h3>
 
-        <ul className='list-disc text-sm mx-4'>
-          <li>Change your name as per the college ID</li>
-          <li>
-            If there is an error in the event participation or result please contact&nbsp;
-            <a className='text-green-500 underline' target='_blank' rel="noreferrer" href='https://wa.me/916238065153'>wa.me/916238065153</a>
-          </li>
-        </ul>
+          <ul className="list-disc text-sm mx-4">
+            <li>Change your name as per the college ID</li>
+            <li>
+              If there is an error in the event participation or result please
+              contact&nbsp;
+              <a
+                className="text-green-500 underline"
+                target="_blank"
+                rel="noreferrer"
+                href="https://wa.me/916238065153"
+              >
+                wa.me/916238065153
+              </a>
+            </li>
+          </ul>
 
-        <PersonalDetails 
-          name={singeData?.personal_details?.name}
-          email={singeData?.personal_details?.Email}
-          year={singeData?.personal_details?.year}
-          dept={singeData?.personal_details?.dept}
-        />
-
-
-        <div className='flex flex-col space-y-4'>
-          <h3>Event Participation Details</h3>
-          <CertificateDetails type="Individual Events Certificates" participant={singeData?.personal_details?.name} 
-             year={singeData?.personal_details?.year} dept={singeData?.personal_details?.dept} 
-          data={{
-            event_1: singeData?.personal_details?.event_1 && { name: singeData?.personal_details?.event_1, result: singeData?.personal_details?.result_1, cert: singeData?.personal_details?.cert_1},
-            event_2: singeData?.personal_details?.event_2 && { name: singeData?.personal_details?.event_2, result: singeData?.personal_details?.result_2, cert: singeData?.personal_details?.cert_2},
-            event_3: singeData?.personal_details?.event_3 && { name: singeData?.personal_details?.event_3, result: singeData?.personal_details?.result_3, cert: singeData?.personal_details?.cert_3},
-            event_4: singeData?.personal_details?.event_4 && { name: singeData?.personal_details?.event_4, result: singeData?.personal_details?.result_4, cert: singeData?.personal_details?.cert_4},
-            event_5: singeData?.personal_details?.event_5 && { name: singeData?.personal_details?.event_5, result: singeData?.personal_details?.result_5, cert: singeData?.personal_details?.cert_5},
-            event_6: singeData?.personal_details?.event_6 && { name: singeData?.personal_details?.event_6, result: singeData?.personal_details?.result_6, cert: singeData?.personal_details?.cert_6},
-          }}/>
-
-          <CertificateDetails type="Group Events Certificates" participant={groupData?.personal_details?.name} 
-             year={groupData?.personal_details?.year} dept={groupData?.personal_details?.dept} id={groupData?.personal_details?.id}
-           data={{
-            event_1: groupData?.personal_details?.event_1 && { name: groupData?.personal_details?.event_1, result: groupData?.personal_details?.result_1, cert: groupData?.personal_details?.cert_1},
-            event_2: groupData?.personal_details?.event_2 && { name: groupData?.personal_details?.event_2, result: groupData?.personal_details?.result_2, cert: groupData?.personal_details?.cert_2},
-            event_3: groupData?.personal_details?.event_3 && { name: groupData?.personal_details?.event_3, result: groupData?.personal_details?.result_3, cert: groupData?.personal_details?.cert_3},
-          }}/>
+          <PersonalDetails
+            onClick={(name, seterror) => {
+              handleEdit(
+                singeData?.personal_details?.Email ||
+                  groupData?.personal_details?.Email,
+                name,
+                singeData,
+                groupData,
+                setsingleData,
+                setgroupData,
+                seterror,
+                setloading,
+                onSubmit
+              );
+            }}
+            loading={loading}
+            name={
+              singeData?.personal_details?.name ||
+              groupData?.personal_details?.name
+            }
+            email={
+              singeData?.personal_details?.Email ||
+              groupData?.personal_details?.Email
+            }
+            year={
+              singeData?.personal_details?.year ||
+              groupData?.personal_details?.year
+            }
+            dept={
+              singeData?.personal_details?.dept ||
+              groupData?.personal_details?.dept
+            }
+          />
+          <div className="w-full flex flex-col space-y-2 justify-center items-center">
+            <p className="text-green-500 underline">
+              No changes required? <span className="text-white">or</span>{" "}
+              Already made the changes!
+            </p>
+            <button
+              className="submit bg-green-500"
+              onClick={() => setshow(true)}
+            >
+              Show certificates
+            </button>
+          </div>
         </div>
-        
-      </div> }
+      )}
 
-      
+      {show && (singeData || groupData) && (
+        <div className="flex flex-col space-y-4 bg-zinc-900 rounded-3xl  px-8  py-6">
+          <div className="flex flex-col space-y-2">
+            <h3>Event Participation Details</h3>
+            <p className="text-green-500 break-words">
+              Please wait sometimes while generating the certificates.
+            </p>
+          </div>
+          <div className="flex flex-col space-y-8">
+            {singeData && (
+              <CertificateDetails
+                type="Individual Events Certificates"
+                participant={singeData?.personal_details?.name}
+                year={singeData?.personal_details?.year}
+                dept={singeData?.personal_details?.dept}
+                data={{
+                  event_1: singeData?.personal_details?.event_1 && {
+                    name: singeData?.personal_details?.event_1,
+                    result: singeData?.personal_details?.result_1,
+                    cert: singeData?.personal_details?.cert_1,
+                  },
+                  event_2: singeData?.personal_details?.event_2 && {
+                    name: singeData?.personal_details?.event_2,
+                    result: singeData?.personal_details?.result_2,
+                    cert: singeData?.personal_details?.cert_2,
+                  },
+                  event_3: singeData?.personal_details?.event_3 && {
+                    name: singeData?.personal_details?.event_3,
+                    result: singeData?.personal_details?.result_3,
+                    cert: singeData?.personal_details?.cert_3,
+                  },
+                  event_4: singeData?.personal_details?.event_4 && {
+                    name: singeData?.personal_details?.event_4,
+                    result: singeData?.personal_details?.result_4,
+                    cert: singeData?.personal_details?.cert_4,
+                  },
+                  event_5: singeData?.personal_details?.event_5 && {
+                    name: singeData?.personal_details?.event_5,
+                    result: singeData?.personal_details?.result_5,
+                    cert: singeData?.personal_details?.cert_5,
+                  },
+                  event_6: singeData?.personal_details?.event_6 && {
+                    name: singeData?.personal_details?.event_6,
+                    result: singeData?.personal_details?.result_6,
+                    cert: singeData?.personal_details?.cert_6,
+                  },
+                }}
+              />
+            )}
 
+            {groupData && (
+              <CertificateDetails
+                type="Group Events Certificates"
+                participant={groupData?.personal_details?.name}
+                year={groupData?.personal_details?.year}
+                dept={groupData?.personal_details?.dept}
+                id={groupData?.personal_details?.id}
+                data={{
+                  event_1: groupData?.personal_details?.event_1 && {
+                    name: groupData?.personal_details?.event_1,
+                    result: groupData?.personal_details?.result_1,
+                    cert: groupData?.personal_details?.cert_1,
+                  },
+                  event_2: groupData?.personal_details?.event_2 && {
+                    name: groupData?.personal_details?.event_2,
+                    result: groupData?.personal_details?.result_2,
+                    cert: groupData?.personal_details?.cert_2,
+                  },
+                  event_3: groupData?.personal_details?.event_3 && {
+                    name: groupData?.personal_details?.event_3,
+                    result: groupData?.personal_details?.result_3,
+                    cert: groupData?.personal_details?.cert_3,
+                  },
+                }}
+              />
+            )}
+          </div>
+        </div>
+      )}
     </div>
-  )
+  );
 }
--- util/supabase.helper.js
+++ util/supabase.helper.js
@@ -1,71 +1,158 @@
 import { supabase } from "supabase";
 
-
-export const matchEvents = (personalDetails, eventDetails) => {
-    eventDetails.forEach(event => {
-        if(event.id == personalDetails.event_1){
-            personalDetails.cert_1 = `${personalDetails.id}${personalDetails.event_1+10}` 
-            personalDetails.event_1 = event.name
-        }else if(event.id == personalDetails.event_2){
-            personalDetails.cert_2 = `${personalDetails.id}${personalDetails.event_2+10}` 
-            personalDetails.event_2 = event.name
-        }else if(event.id == personalDetails.event_3){
-            personalDetails.cert_3 = `${personalDetails.id}${personalDetails.event_3+10}` 
-            personalDetails.event_3 = event.name
-        }else if(event.id == personalDetails.event_4){
-            personalDetails.cert_4 = `${personalDetails.id}${personalDetails.event_4+10}` 
-            personalDetails.event_4 = event.name
-        }else if(event.id == personalDetails.event_5){
-            personalDetails.cert_5 = `${personalDetails.id}${personalDetails.event_5+10}` 
-            personalDetails.event_5 = event.name
-        }else if(event.id == personalDetails.event_6){
-            personalDetails.cert_6 = `${personalDetails.id}${personalDetails.event_6+10}` 
-            personalDetails.event_6 = event.name
-        }
-    })
-    return personalDetails
-}
-
-const fetchSingle = async (email, setsingleData, seterror) => {
-
-    const personalDetails = await supabase.from('single_participation').select().eq('Email', email)
-    const eventDetails = await supabase.from('single_events').select()
-    if (personalDetails?.error || eventDetails?.error) {
-        seterror(true)
-        return
+export async function handleEdit(
+  email,
+  name,
+  singeData,
+  groupData,
+  setsingleData,
+  setgroupData,
+  seterror,
+  setloading,
+  onSubmit
+) {
+  seterror(false);
+  const names = name.split(" ");
+  if (names?.length > 0) {
+    let flag = 0;
+    names.forEach((item) => {
+      const regexp = new RegExp(item, "i");
+      email.search(regexp) === 0 && (flag = 1);
+    });
+    if (flag === 0) {
+      seterror(true);
+      return;
     }
-    const finalDetails = matchEvents(personalDetails?.data[0], eventDetails?.data)
-    setsingleData({
-        personal_details: finalDetails,
-    })
-
-}
-
-const fetchGroup = async (email, setgroupData, seterror) => {
-
-    const personalDetails = await supabase.from('group_participation').select().eq('Email', email)
-    const eventDetails = await supabase.from('group_events').select()
-    if (personalDetails?.error || eventDetails?.error) {
-        seterror(true)
-        return
+    setloading(true);
+    if (singeData) {
+      await supabase
+        .from("single_participation")
+        .update({ name: name })
+        .eq("Email", email);
     }
-    const finalDetails = matchEvents(personalDetails?.data[0], eventDetails?.data)
-    setgroupData({
-        personal_details: finalDetails,personalDetails
-    })
 
+    if (groupData) {
+      await supabase
+        .from("group_participation")
+        .update({ name: name })
+        .eq("Email", email);
+    }
+    setloading(false);
+
+    setsingleData(null);
+    setgroupData(null);
+
+    onSubmit({ email: email });
+  }
 }
 
-export async function fetchSingleEvents(setsingle){
-    const single = await supabase.from('single_events').select()
-    setsingle(single.data)
+export function matchEvents(personalDetails, eventDetails) {
+  eventDetails.forEach((event) => {
+    if (event.id == personalDetails.event_1) {
+      personalDetails.cert_1 = `${personalDetails.id}${
+        personalDetails.event_1 + 10
+      }`;
+      personalDetails.event_1 = event.name;
+    } else if (event.id == personalDetails.event_2) {
+      personalDetails.cert_2 = `${personalDetails.id}${
+        personalDetails.event_2 + 10
+      }`;
+      personalDetails.event_2 = event.name;
+    } else if (event.id == personalDetails.event_3) {
+      personalDetails.cert_3 = `${personalDetails.id}${
+        personalDetails.event_3 + 10
+      }`;
+      personalDetails.event_3 = event.name;
+    } else if (event.id == personalDetails.event_4) {
+      personalDetails.cert_4 = `${personalDetails.id}${
+        personalDetails.event_4 + 10
+      }`;
+      personalDetails.event_4 = event.name;
+    } else if (event.id == personalDetails.event_5) {
+      personalDetails.cert_5 = `${personalDetails.id}${
+        personalDetails.event_5 + 10
+      }`;
+      personalDetails.event_5 = event.name;
+    } else if (event.id == personalDetails.event_6) {
+      personalDetails.cert_6 = `${personalDetails.id}${
+        personalDetails.event_6 + 10
+      }`;
+      personalDetails.event_6 = event.name;
+    }
+  });
+  return personalDetails;
 }
 
-export async function fetchGroupEvents(setgroup){
-    const group = await supabase.from('group_events').select()
-    setgroup(group.data)
+export async function fetchByEmail(
+  email,
+  setsingleData,
+  setgroupData,
+  seterror,
+  setloading
+) {
+  setloading(true);
+  const e1 = await fetchSingle(email, setsingleData);
+  const e2 = await fetchGroup(email, setgroupData);
+  e1 && e2 && seterror(true);
+  setloading(false);
 }
 
+export async function fetchSingle(email, setsingleData) {
+  const personalDetails = await supabase
+    .from("single_participation")
+    .select()
+    .eq("Email", email);
+  const eventDetails = await supabase.from("single_events").select();
+  if (
+    personalDetails?.error ||
+    personalDetails?.data?.length === 0 ||
+    eventDetails?.error
+  ) {
+    setsingleData(null);
+    return true;
+  }
+  const finalDetails = matchEvents(
+    personalDetails?.data[0],
+    eventDetails?.data
+  );
+  setsingleData({
+    personal_details: finalDetails,
+  });
+}
+
+export async function fetchGroup(email, setgroupData) {
+  const personalDetails = await supabase
+    .from("group_participation")
+    .select()
+    .eq("Email", email);
+  const eventDetails = await supabase.from("group_events").select();
+  if (
+    personalDetails?.error ||
+    personalDetails?.data?.length === 0 ||
+    eventDetails?.error
+  ) {
+    setgroupData(null);
+    return true;
+  }
+  const finalDetails = matchEvents(
+    personalDetails?.data[0],
+    eventDetails?.data
+  );
+  setgroupData({
+    personal_details: finalDetails,
+    personalDetails,
+  });
+}
+
+export async function fetchSingleEvents(setsingle) {
+  const single = await supabase.from("single_events").select();
+  setsingle(single.data);
+}
+
+export async function fetchGroupEvents(setgroup) {
+  const group = await supabase.from("group_events").select();
+  setgroup(group.data);
+}
 
 // const updateEvent = async () => {
 //     const part = await supabase.from('group_participation').select('Email,event')
@@ -85,62 +172,56 @@ export async function fetchGroupEvents(setgroup){
 // }
 
 export const fetchAllSingleData = async (setdata, setsingle, setloading) => {
-    setloading(true)
-    const personalDetails = await supabase.from('single_participation').select().order('id', { ascending: true })
-    const eventDetails = await supabase.from('single_events').select()
-    if (personalDetails?.error || eventDetails?.error) {
-        console.log(personalDetails?.error)
-        return
-    }
-    let ar =[]
-    personalDetails?.data?.forEach(ele => {
-        const finalDetails = matchEvents(ele, eventDetails?.data)
-        ar.push(finalDetails)
-    })
-    setsingle(eventDetails?.data)
-    setdata(ar)
-    setloading(false)
-}
+  setloading(true);
+  const personalDetails = await supabase
+    .from("single_participation")
+    .select()
+    .order("id", { ascending: true });
+  const eventDetails = await supabase.from("single_events").select();
+  if (personalDetails?.error || eventDetails?.error) {
+    console.log(personalDetails?.error);
+    return;
+  }
+  let ar = [];
+  personalDetails?.data?.forEach((ele) => {
+    const finalDetails = matchEvents(ele, eventDetails?.data);
+    ar.push(finalDetails);
+  });
+  setsingle(eventDetails?.data);
+  setdata(ar);
+  setloading(false);
+};
 
 export const fetchAllGroupData = async (setdata, setgroup, setloading) => {
-    setloading(true)
-    const personalDetails = await supabase.from('group_participation').select()
-    const eventDetails = await supabase.from('group_events').select()
-    if (personalDetails?.error || eventDetails?.error) {
-        console.log(personalDetails?.error)
-        return
-    }
-    let ar =[]
-    personalDetails?.data?.forEach(ele => {
-        const finalDetails = matchEvents(ele, eventDetails?.data)
-        ar.push(finalDetails)
-    })
-    setgroup(eventDetails?.data)
-    setdata(ar)
-    setloading(false)
-}
+  setloading(true);
+  const personalDetails = await supabase.from("group_participation").select();
+  const eventDetails = await supabase.from("group_events").select();
+  if (personalDetails?.error || eventDetails?.error) {
+    console.log(personalDetails?.error);
+    return;
+  }
+  let ar = [];
+  personalDetails?.data?.forEach((ele) => {
+    const finalDetails = matchEvents(ele, eventDetails?.data);
+    ar.push(finalDetails);
+  });
+  setgroup(eventDetails?.data);
+  setdata(ar);
+  setloading(false);
+};
 
+export async function uploadSign(image, user, router) {
+  const { data, error } = await supabase.storage
+    .from("signature")
+    .upload(`${user}.png`, image, {
+      cacheControl: "3600",
+      upsert: true,
+    });
 
-export const fetchByEmail = async (email, setsingleData, setgroupData, seterror, setloading) => {
-    setloading(true)
-    await fetchSingle(email, setsingleData, seterror)
-    await fetchGroup(email, setgroupData, seterror)
-    setloading(false)
-}
-  
-
-export async function uploadSign(image, user, router){
-    const { data, error } = await supabase.storage
-      .from('signature')
-      .upload(`${user}.png`, image, {
-        cacheControl: '3600',
-        upsert: true
-      })
-
-    if (error) {
-     console.log(error);   
-    }else{
-        console.log('s');
-        router.reload()
-    }
+  if (error) {
+    console.log(error);
+  } else {
+    console.log("s");
+    router.reload();
+  }
 }
